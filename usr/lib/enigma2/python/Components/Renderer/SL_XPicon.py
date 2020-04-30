788b46031ab41ad9d35feff8acb8a6cb1abfb982
rogerSSD <rogerd@hotmail.de>
2020-04-30 16:10:38 +0200

    Update 2

--- usr/lib/enigma2/python/Components/Renderer/SL_XPicon.py
+++ usr/lib/enigma2/python/Components/Renderer/SL_XPicon.py
@@ -7,6 +7,10 @@ from enigma import ePixmap
 from enigma import iServiceInformation, iPlayableService, iPlayableServicePtr
 from Tools.Directories import fileExists, SCOPE_SKIN_IMAGE, SCOPE_CURRENT_SKIN, resolveFilename
 
+# For SNP
+from ServiceReference import ServiceReference
+import re, unicodedata
+
 class SL_XPicon(Renderer):
 	searchPaths = ('/media/usb/XPicons/%s/','/media/usb/%s/','/%s/','/%sx/','/usr/share/enigma2/XPicons/%s/','/usr/share/enigma2/%s/','/usr/%s/','/media/hdd/XPicons/%s/','/media/hdd/%s/')
 
@@ -32,6 +36,7 @@ class SL_XPicon(Renderer):
 		if self.instance:
 			pngname = ""
 			if what[0] != self.CHANGED_CLEAR:
+				self.instance.show()
 				sname = self.source.text
 				pos = sname.rfind(':')
 				if pos != -1:
@@ -44,26 +49,37 @@ class SL_XPicon(Renderer):
 						fields = sname.split('_', 3)
 						if len(fields) > 2 and fields[2] != '2': #fallback to 1 for tv services with nonstandard servicetypes
 							fields[2] = '1'
-						if fields[0] == '4097': #fallback to 1 for IPTV streams
+						if fields[0] == '4097' or fields[0] == '5002': #fallback to 1 for IPTV streams
 							fields[0] = '1'
 						pngname = self.findPicon('_'.join(fields))
+					if not pngname: # picon by channel name
+						name = ServiceReference(self.source.text).getServiceName()
+						name = unicodedata.normalize('NFKD', unicode(name, 'utf_8', errors='ignore')).encode('ASCII', 'ignore')
+						name = re.sub('[^a-z0-9]', '', name.replace('&', 'and').replace('+', 'plus').replace('*', 'star').lower())
+						if len(name) > 0:
+							pngname = self.findPicon(name)
+							if not pngname and len(name) > 2 and name.endswith('hd'):
+								pngname = self.findPicon(name[:-2])
 					if pngname != "":
 						self.nameCache[sname] = pngname
-			if pngname == "": # no picon for service found
-				pngname = self.nameCache.get("default", "")
-				if pngname == "": # no default yet in cache..
-					pngname = self.findPicon("picon_default")
-					if pngname == "":
-						tmp = resolveFilename(SCOPE_CURRENT_SKIN, "picon_default.png")
-						if fileExists(tmp):
-							pngname = tmp
-						else:
-							pngname = resolveFilename(SCOPE_SKIN_IMAGE, "skin_default/picon_default.png")
-					self.nameCache["default"] = pngname
-			if self.pngname != pngname:
-				self.instance.setScale(1)
-				self.instance.setPixmapFromFile(pngname)
-				self.pngname = pngname
+				if pngname == "": # no picon for service found
+					pngname = self.nameCache.get("default", "")
+					if pngname == "": # no default yet in cache..
+						pngname = self.findPicon("picon_default")
+						if pngname == "":
+							tmp = resolveFilename(SCOPE_CURRENT_SKIN, "picon_default.png")
+							if fileExists(tmp):
+								pngname = tmp
+							else:
+								pngname = resolveFilename(SCOPE_SKIN_IMAGE, "skin_default/picon_default.png")
+						self.nameCache["default"] = pngname
+				if self.pngname != pngname:
+					self.instance.setScale(1)
+					self.instance.setPixmapFromFile(pngname)
+					self.pngname = pngname
+			else:
+				self.pngname = ""
+				self.instance.hide()
 
 	def findPicon(self, serviceName):
 		for path in self.searchPaths:
